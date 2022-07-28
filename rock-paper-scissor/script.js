2b0bf70ea0749242d9d5b2660c0f9eb323457a07
Mohamed Wasim Mohamed Akram <wasim.mohamed@ug.bilkent.edu.tr>
2022-07-28 12:15:36 +0300

    Rock Paper Scissor Initial

---
+++ rock-paper-scissor/script.js
@@ -0,0 +1,27 @@
+// console.log(random, choice[random]);
+
+function getComputerChoice() {
+    const choice = ["Rock", "Paper", "Scissor"];
+    const random = Math.floor(Math.random() * choice.length);
+    return choice[random];
+}
+
+let playerSelection = prompt("Rock, Paper or Scissor?");
+
+function rockPaperScissor(playerSelection, computerSelection){
+    if((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() == 'scissor') 
+    || (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock')
+    || (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'paper')){
+        console.log(`User played ${playerSelection} and Computer played ${computerSelection}: User wins`);
+    }
+    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        console.log(`User played ${playerSelection} and Computer played ${computerSelection}: Draw`);
+    }else if(playerSelection.toLowerCase() !== 'rock' || playerSelection.toLowerCase() !== 'scissor' || playerSelection.toLowerCase() !== 'paper'){
+        alert("Incorrect Input");
+    }
+    else{
+        console.log(`User played ${playerSelection} and Computer played ${computerSelection}: Computer wins`);
+    }
+}
+
+rockPaperScissor(playerSelection, getComputerChoice());
